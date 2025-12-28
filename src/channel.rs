cd08058e028d038f8dd79622063d2199413d863d
JKrishnaD <jayak5063@gmail.com>
2025-12-28 23:42:50 +0530

    channel exercises for learning

--- src/channel.rs
+++ src/channel.rs
@@ -1,8 +1,8 @@
-// use std::{
-//     sync::mpsc,
-//     thread::{self, sleep},
-//     time::Duration,
-// };
+use std::{
+    sync::mpsc,
+    thread::{self, sleep},
+    time::Duration,
+};
 
 // fn main() {
 //     println!("This contains my learnings");
@@ -103,3 +103,148 @@
 //             .recv_timeout() -> blocking
 //             .iter() -> blocking
 //             .try_recv() -> non-blocking
+
+fn main() {
+    // let (tx, rx) = mpsc::channel();
+    // let data = String::from("hello");
+
+    // tx.send(data).unwrap();
+    // tx.send(data).unwrap();
+    // here it is failing because it is failing because the data is already moved
+    // it's ownership to the reciever by first tx.send()
+    // so when are tryig to send it again it throws the error as use of a moved value
+    // let (tx, rx) = mpsc::channel();
+
+    // thread::spawn(move || {
+    //     tx.send(1).unwrap();
+    // });
+
+    // for msg in rx {
+    //     println!("{}", msg);
+    // }
+    //The loop terminates because the sender is dropped when the spawned thread exits, which closes the channel.
+
+    // let (tx1, rx1) = mpsc::sync_channel(10);
+
+    // // A bounded channel enforces backpressure by blocking senders when the buffer is full.
+    // for i in 0..1_000_000 {
+    //     tx1.send(i).unwrap();
+    // }
+
+    // for msg in rx1 {
+    //     println!("Message from bound: {}", msg);
+    // }
+
+    // drop(tx1);
+
+    // let (tx2, rx2) = mpsc::channel::<i32>();
+
+    // for i in 0..5 {
+    //     let tx_clone = tx2.clone();
+    //     thread::spawn(move || {
+    //         tx_clone.send(i).unwrap();
+    //     });
+    // }
+
+    // drop(tx2);
+    // let mut data: Vec<i32> = vec![];
+
+    // for msg in rx2 {
+    //     data.push(msg);
+    // }
+
+    //println!("message data: {:?}", data);
+    // why mpsc is more better fundamentally than Mutex and Arc
+    // mpsc is better suited for async because it models ownership transfer and non-blocking coordination,
+    // whereas Mutex relies on blocking and shared state, which breaks async execution
+
+    // 1 - write a program a threads sends a string the main thread prints it and sender cannot use the string after sending
+    // let (tx, rx) = mpsc::channel::<String>();
+    // let msg = String::from("Hello from the sender");
+    // let handle = thread::spawn(move || tx.send(msg));
+
+    // println!("Recieved message: {}", rx.recv().unwrap());
+
+    // let _ = handle.join().unwrap();
+
+    // 2 - One thread sends 3 messages, Main thread iterates over rx, the loop must terminate naturally
+    // let (tx, rx) = mpsc::channel::<i32>();
+    // let (tx, rx) = mpsc::sync_channel::<i32>(5);
+    // let mut handles: Vec<JoinHandle<()>> = vec![];
+    // when it comes to the bound buffers it can buffer atmost messages mentioned in bound
+    // if buffer is full then send() blocks until the reciever removes all the existing messages
+    // this is called backpressure
+    //
+    // unlike channels send() in sync_channle() can block the sender thread in mid execution
+
+    // Each producer must own it's own sender so each of them to be cloned before the threads even spawn
+    // and the channels which are opened must be explicitly closed to terminate the loop
+    // for i in 0..5 {
+    //     for _ in 0..3 {
+    //         let tx_clone = tx.clone();
+    //         let handle = thread::spawn(move || {
+    //             tx_clone.send(i).unwrap();
+    //         });
+    //         handles.push(handle);
+    //     }
+    // }
+    // closing the chanels
+    // drop(tx);
+
+    // println!("All threads are eliminated");
+
+    // in here we need to handle the messages first before handling the threads
+    // so by the time the 5 messages bound reached to we need to clean the rx so that new messags can come in
+    // if the thread handling is done first than rx then the execution will stop
+    // because the channel is never cleared which leads to "DEADLOCK"
+    // for msg in rx {
+    //     println!("messages:{}", msg)
+    // }
+
+    // for handle in handles {
+    //     handle.join().unwrap()
+    // }
+
+    // println!("All message are printed");
+
+    //Write a program where: Receiver calls recv(), No sender exists for 3 seconds, Then a sender sends a message
+    // let (tx, rx) = mpsc::channel::<String>();
+    // let handle = thread::spawn(move || {
+    //     sleep(Duration::from_secs(3));
+    //     tx.send("Hello!".to_string()).unwrap();
+    // });
+    // println!("Message sent");
+
+    // let msg = rx.recv().unwrap();
+    // handle.join().unwrap();
+    // println!("Recieved Message: {:?}", msg)
+
+    // write code where producer generates data very quickly and consumer processes data very slowly
+    // the major idea behind this is that we need to use the bound channels so that we can
+    // restrict the messages limit from the sender.
+
+    let (tx, rx) = mpsc::sync_channel::<i32>(2);
+
+    // what really happening here is that
+    // - we have created a situation where we have a bound channel with buffer capacity 2
+    // - in the loop we are sending messages with the limit 10
+    // - while recieving them we are printing each message and waiting for 2 secs
+    // - rx will wait to get all the message but after gettign 2 messages the
+    // - sender will stop as it's bound is 2 and wait until the reciever buffer is clear
+    // - so the reciever create a room to let the 3rd mesaage by clearing the first message
+    // - and this continues until the loop ends.
+    let handle = thread::spawn(move || {
+        for i in 0..10 {
+            println!("Producing {}", i);
+            tx.send(i).unwrap();
+            println!("sent {}", i);
+        }
+    });
+
+    for msg in rx {
+        println!("Message Recieved: {:?}", msg);
+        thread::sleep(Duration::from_secs(2));
+    }
+
+    handle.join().unwrap();
+}
